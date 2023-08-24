61b8067e8053cb99d7611e7d85199f1d0743558e
Ld-monkey <lfourteau91@gmail.com>
2023-08-24 15:39:28 +0200

    create pool rather than client

--- app/models/client.js
+++ app/models/client.js
@@ -1,6 +1,15 @@
-const { Client } = require('pg');
+const { Pool } = require('pg');
 
-const client = new Client();
-client.connect();
+const pool = new Pool({
+  min: 0,
+  max: 10,
+  createTimeoutMillis: 8000,
+  acquireTimeoutMillis: 8000,
+  idleTimeoutMillis: 8000,
+  reapIntervalMillis: 1000,
+  createRetryIntervalMillis: 100,
+});
 
-module.exports = client;
+pool.connect();
+
+module.exports = pool;
